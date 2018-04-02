e5ba18b20c493da57f5baf78056cf78576b33e84
Yalchin Aliyev <yalchin.aliyev@outlook.com>
2018-04-03 01:22:54 +0300

    Add semaphore prefixe to common.h

--- common.h
+++ common.h
@@ -5,6 +5,18 @@
 #define BUFSIZE 100
 #define KEYWORD_SIZE 128
 
+// semaphore sufixes for queue state
+#define QS_SEM_MUTEX "qs_sem_mutex"
+
+// semaphore sufixes for request queue
+#define RQQ_SEM_MUTEX "rqq_sem_mutex"
+#define RQQ_SEM_FULL "rqq_sem_full"
+
+// semaphore sufixes for result queue
+#define RSQ_SEM_MUTEX "rsq_sem_mutex_"
+#define RSQ_SEM_FULL "rsq_sem_full_"
+#define RSQ_SEM_EMPTY "rsq_sem_empty_"
+
 struct result_queue_struct {
 	int buf[BUFSIZE];
 	int in;
@@ -13,7 +25,7 @@ struct result_queue_struct {
 };
 
 struct request_queue_struct {
-	char buf[NUM_OF_CLIENTS][KEYWORD_SIZE];
+	char buf[NUM_OF_CLIENTS][KEYWORD_SIZE + 1];
 	int in;
 	int out;
 	int count;
